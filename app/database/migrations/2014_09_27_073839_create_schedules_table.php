51c6c125b5350635aa19d3b8565442b7e801cd80
wern <ancheta.wern@gmail.com>
2014-09-27 07:54:59 +0800

    add migrations for creating intervals and schedules table

---
+++ app/database/migrations/2014_09_27_073839_create_schedules_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSchedulesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('schedules', function($table)
+        {
+            $table->increments('id');
+            $table->string('interval_id');
+            $table->integer('user_id');
+            $table->string('name');
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
