f635eb7026920074d6273fe1af03e1e57fdb9722
wern <ancheta.wern@gmail.com>
2014-09-28 10:34:12 +0800

    add migration for adding condition and hours field to intervals table

---
+++ app/database/migrations/2014_09_28_075709_add_condition_and_hours_to_intervals_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddConditionAndHoursToIntervalsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::table('intervals', function($table)
+        {
+            $table->string('rule');
+            $table->integer('hours');
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
